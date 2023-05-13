251757b7311aebb17ce861b8f99a67650531d499
Elvis Miranda <elvis.miranda@live.com>
2023-05-13 03:49:45 -0400

    Add collections sample

--- src/main/kotlin/beyond/Book.kt
+++ src/main/kotlin/beyond/Book.kt
@@ -5,10 +5,9 @@ data class Result(val title: String, val author: String)
 class Book (val title: String, val author: String, val year: Int) {
 
     fun getAuthorAndTitle(): Result = Result(title, author)
-    fun getDetails() = Triple(this.title, author, year)
+    fun getDetails() = Triple(title, author, year)
 
 }
-
 
 fun main() {
     val book = Book("Book 1", "Developer", 2018)
@@ -16,4 +15,12 @@ fun main() {
     val (title, author, year) = book.getDetails()
 
     println("Here is your book $title written by $author in $year")
+
+    val allBooks: Set<String> = setOf("Macbeth", "Romeo and Juliet", "Hamlet", "A Midsummer Night's Dream")
+    val library: Map<String, Set<String>> = mapOf("Shakespeare" to allBooks)
+
+    println(library.any { it.value.contains("Hamlet") })
+
+    val moreBooks = mutableMapOf("Author 2" to "Book 2")
+    println(moreBooks.getOrPut("Author 2") { "No Books" })
 }
