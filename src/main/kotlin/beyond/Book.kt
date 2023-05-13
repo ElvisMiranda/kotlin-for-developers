a0746cc027badfd7bb2c12172a8dc378ca096d43
Elvis Miranda <elvis.miranda@live.com>
2023-05-13 04:00:00 -0400

    Add constants

--- src/main/kotlin/beyond/Book.kt
+++ src/main/kotlin/beyond/Book.kt
@@ -2,11 +2,21 @@ package beyond
 
 data class Result(val title: String, val author: String)
 // Pair<String,String> as result is also useful
+
+const val MAX_BOOKS = 10
+
 class Book (val title: String, val author: String, val year: Int) {
+
+    companion object {
+        const val BASE_URL = "http://sample/"
+    }
 
     fun getAuthorAndTitle(): Result = Result(title, author)
     fun getDetails() = Triple(title, author, year)
 
+    fun canBorrow(hasBooks: Int) = hasBooks < MAX_BOOKS
+
+    fun printUrl() = println("$BASE_URL$title.html")
 }
 
 fun main() {
@@ -23,4 +33,8 @@ fun main() {
 
     val moreBooks = mutableMapOf("Author 2" to "Book 2")
     println(moreBooks.getOrPut("Author 2") { "No Books" })
+
+    println(book.canBorrow(10))
+    println(book.printUrl())
+
 }
