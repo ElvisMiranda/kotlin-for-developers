df587576e2e29665d1c946a6d7be6b10a270af51
Elvis Miranda <elvis.miranda@live.com>
2023-05-14 04:11:04 -0400

    Add high order functions

--- src/main/kotlin/functional/Functional.kt
+++ src/main/kotlin/functional/Functional.kt
@@ -11,10 +11,10 @@ enum class Directions {
 class Game {
     var path = mutableListOf(Directions.START)
 
-    val north = { path.add(Directions.NORTH) }
+    private val north = { path.add(Directions.NORTH) }
     val south = { path.add(Directions.SOUTH) }
     val east = { path.add(Directions.EAST) }
-    val west : () -> Unit = { path.add(Directions.WEST) }
+    val west = { path.add(Directions.WEST) }
     val end : () -> Boolean = {
         path.add(Directions.END)
         println("Game Over: $path")
@@ -22,18 +22,34 @@ class Game {
 
         false
     }
+
+    private fun move(where: () -> Boolean) {
+        where.invoke()
+    }
+
+    fun makeMove(direction: String?) {
+        if (direction.equals("n")) move(north)
+        else if (direction.equals("s")) move(south)
+        else if (direction.equals("e")) move(east)
+        else if (direction.equals("w")) move(west)
+        else move(end)
+    }
 }
 
 fun main() {
     val game = Game()
 
-    println(game.path)
-    game.north()
-    game.south()
-    game.east()
-    game.west()
-    game.end()
+//    println(game.path)
+//    game.north()
+//    game.south()
+//    game.east()
+//    game.west()
+//    game.end()
+//
+//    println(game.path)
 
-    println(game.path)
-
+    while (true) {
+        print("Enter a direction: n/s/e/w:")
+        game.makeMove(readlnOrNull())
+    }
 }
